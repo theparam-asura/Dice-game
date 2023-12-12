07fd3d5af90412cb58e6c329199980dfd4794c21
theparam-asura <127755592+theparam-asura@users.noreply.github.com>
2023-12-13 01:08:23 +0530

    Update index.js
    Signed-off-by: theparam-asura <127755592+theparam-asura@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var randomNumber1=Math.random();
 randomNumber1=randomNumber1*6;
 randomNumber1=Math.floor(randomNumber1)+1;
 var randomDiceImage="dice"+randomNumber1+".png";
-var randomImageSource="./"+randomDiceImage;
+var randomImageSource=randomDiceImage;
 var image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomImageSource);
 
@@ -10,7 +10,7 @@ image1.setAttribute("src",randomImageSource);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "./dice" + randomNumber2 + ".png";
+var randomImageSource2 = "dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
